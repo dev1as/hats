79af4565651c5da2fdd77743f752c5323d8c7290
dev1as <168581503+dev1as@users.noreply.github.com>
2024-05-02 21:13:17 +0200

    data.mjs

--- data/data.mjs
+++ data/data.mjs
@@ -14,7 +14,7 @@ const data = [
 
     {
         id: 2,
-        imgLocker: `./imgs/locker2.jpg`,
+        imgLocker: `./imgs/locker2.png`,
         title: `offer 2`,
         description: `
            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
@@ -28,30 +28,6 @@ const data = [
         id: 3,
         imgLocker: `./imgs/locker2.jpg`,
         title: `offer 3`,
-        description: `
-           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        `,
-        device: `All`,
-        category: `Emait & Submit`, 
-        date: `3 April, 2022`,
-        link_locker: `https://installyourfiles.com/1680921`,
-    },
-{
-        id: 4,
-        imgLocker: `./imgs/locker2.jpg`,
-        title: `offer 4`,
-        description: `
-           aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-        `,
-        device: `All`,
-        category: `Emait & Submit`, 
-        date: `3 April, 2022`,
-        link_locker: `https://installyourfiles.com/1680921`,
-    },
-{
-        id: 5,
-        imgLocker: `./imgs/locker2.jpg`,
-        title: `offer 5`,
         description: `
            aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         `,
